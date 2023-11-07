44bd370c852c4d4924cd5bf9968718a226394ce5
Tobetiger <iamtobeconfidential@gmail.com>
2023-11-07 14:54:29 +0100

    Auth callback page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const Page = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  return <div>{user?.id}</div>;
+};
+export default Page;
